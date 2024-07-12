23e83d6ab668e0163189e76ae43e499059dd7894
keka <keka@conco.Dlink>
2024-07-12 16:34:29 +0300

    feat: add on esc close and form reset

--- js/form.js
+++ js/form.js
@@ -1,20 +1,39 @@
+import { isEscapeKey } from './util.js';
+
 const fileUpload = document.querySelector('#upload-file');
 const popupUpload = document.querySelector('.img-upload__overlay');
 const closeButton = document.querySelector('#upload-cancel');
+const tagsField = document.querySelector('.text__hashtags');
+const photoComment = document.querySelector('.text__description');
 
 fileUpload.addEventListener('change', () => {
   popupUpload.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
 });
+
+const resetForm = () => {
+  fileUpload.value = '';
+  tagsField.value = '';
+  photoComment.value = '';
+};
 
 const closeForm = () => {
   popupUpload.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  resetForm();
 };
 
 closeButton.addEventListener('click', () => {
   closeForm();
 });
 
-// Закрытие формы редактирования изображения производится либо нажатием на кнопку #upload-cancel, либо нажатием клавиши Esc.
-// Элементу .img-upload__overlay возвращается класс hidden. У элемента body удаляется класс modal-open
+function onDocumentKeydown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeForm();
+  }
+}
+
+//onDocumentKeydown сделать универсальным и вынести в утилс?
