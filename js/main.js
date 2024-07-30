36f7ba0c2fd635ba9ea154533b54e43838484375
keka <keka@conco.Dlink>
2024-07-30 17:54:48 +0300

    feat: add initial data module

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
 import { renderGallery } from './gallery.js';
+import { fetchData } from './fetch-initial-data.js';
 import './form.js';
 
-
-fetch('https://28.javascript.htmlacademy.pro/kekstagram/data')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then((posts) => renderGallery(posts))
-  .catch((err) => {
-    throw new Error(err);
-  });
+fetchData().then((pictures) => {
+  renderGallery(pictures);
+});
